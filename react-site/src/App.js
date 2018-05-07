bc51ff1101d8d2d5645334185c148b2d04ab860e
HvNelric <37754127+HvNelric@users.noreply.github.com>
2018-05-07 07:31:04 +0200

    first up
    lundi 7 mai
    07h30

---
+++ react-site/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css'
+import index01 from './img/index01.png';
+
+import NavbarMod from './NavbarMod';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+        <header>
+            <NavbarMod />
+            <div className="container-fluid">
+                <div className="row top-container">
+                    <img src={index01} alt="" className="img-index"/>
+                    <button className="btn btn-info btn-join">Inscrivez-vous</button>
+                </div>
+            </div>
+        </header>
+        <div className="container-fluid" id="page-container">
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
