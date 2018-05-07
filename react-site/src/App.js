81fbabbb1f83048441b6bda770d8f842a19beec4
HvN <37754127+HvNelric@users.noreply.github.com>
2018-05-07 17:33:23 +0200

    etapes
    lundi 7 mai
    17h33

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css'
 import index01 from './img/index01.png';
-
+import recherche from './img/recherche.svg';
+import chat from './img/chat.svg';
+import connect from './img/connect.svg';
 import NavbarMod from './NavbarMod';
 
 class App extends Component {
@@ -18,9 +20,42 @@ class App extends Component {
             </div>
         </header>
         <div className="container-fluid" id="page-container">
-            <div className="row row-test">
-
-            </div>
+                <div className="row row-etapes">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 col-md-4 p-4 text-center">
+                                <img className="forme-hexa" src={recherche} alt=""/>
+                                <h4 className="text-white mt-3 step-h4">Etape01</h4>
+                                <div className="border-conteneur">
+                                    <div className="border-bottom"></div>
+                                </div>
+                                <p className="text-white">
+                                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                                </p>
+                            </div>
+                            <div className="col-12 col-md-4 p-4 text-center">
+                                <img className="forme-hexa" src={chat} alt=""/>
+                                <h4 className="text-white mt-3 step-h4">Etape02</h4>
+                                <div className="border-conteneur">
+                                    <div className="border-bottom"></div>
+                                </div>
+                                <p className="text-white">
+                                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                                </p>
+                            </div>
+                            <div className="col-12 col-md-4 p-4 text-center">
+                                <img className="forme-hexa" src={connect} alt=""/>
+                                <h4 className="text-white mt-3 step-h4">Etape01</h4>
+                                <div className="border-conteneur">
+                                    <div className="border-bottom"></div>
+                                </div>
+                                <p className="text-white">
+                                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
         </div>
     );
