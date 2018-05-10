cc28a832b8f321e17eb98bf0b2cdbc536d616d92
kevin92320 <cuillandrek@gmail.com>
2018-05-10 10:38:11 +0200

    page principale

---
+++ kevin/index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+
+	 <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- Mon style -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Black+Han+Sans" rel="stylesheet">
+</head>
+<body>
+
+						                                        <!-- Destinations -->
+
+<section id="destinations">
+	<div class="container text-center">
+    <h2 class="font-30"><span>D</span>estinations populaires</h2><br><br>
+    <h3 class="">Faites de votre voyage une expérience amusante, passionnante et inoubliable</h3><br><br>
+      <div class="row "><br>
+
+        <div class="col-md-4"> 
+          <img class="card-img-top" src="images/marchenoel.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Voyages toutes générations</h4>
+            <h5 class="card-title">Marché de noël à Strasbourg</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text pull-right"><small class="text-muted">Voir détails ></small></p><br>
+          </div><br><br>
+        </div><br>
+
+        <div class="col-md-4">         
+          <img class="card-img-top" src="images/groupe2.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Hors du commun</h5>
+            <h6 class="card-title">Découverte de la dune du Pila</h6>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text pull-right"><small class="text-muted">Voir détails ></small></p><br>
+          </div>
+        </div><br>
+          
+        <div class="col-md-4">         
+          <img class="card-img-top" src="images/loire.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Escapade</h5>
+            <h6 class="card-title">Spécial chateaux de la Loire</h6>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text pull-right"><small class="text-muted">Voir détails ></small></p><br>
+          </div>
+        </div><br>
+      </div>
+
+      <div class="row "><br>
+        <div class="col-md-4"> 
+          <img class="card-img-top" src="images/nice.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">City Break</h5>
+            <h6 class="card-title">Découvrir Nice</h6>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text pull-right"><small class="text-muted">Voir détails ></small></p><br>
+          </div><br><br>
+        </div><br>
+
+        <div class="col-md-4">         
+          <img class="card-img-top" src="images/alpes.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Montagnes</h5>
+            <h6 class="card-title">Faire du Ski dans les Alpes</h6>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text pull-right"><small class="text-muted">Voir détails ></small></p><br>
+          </div>
+        </div><br>
+          
+              
+        <div class="col-md-4">         
+          <img class="card-img-top" src="images/groupe3.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Randonnées</h5>
+            <h6 class="card-title">Faire le GR20 en Corse</h6>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text pull-right"><small class="text-muted">Voir détails ></small></p><br>
+           </div>
+        </div><br>
+      </div>
+  </div><br>
+
+  <div class="container text-center">
+    <a class="btn btn-primary" href="" >Trouver votre destination</a>
+  </div>       
+</section>
+
+                                              <!-- GROUPE -->
+
+<section id="groupe">
+  <div class="container text-center">
+    <h2 class="font-30"><span>C</span>es groupes n'attendent plus que vous !</h2><br><br>
+  </div>
+  <div class="container text-center">
+    <div class="row "><br>
+      <div class="col-md-4"> 
+        <div class="card">
+          <p class=""><small class=""></small></p><br><br>
+          <img class="card-img" src="images/groupe2.jpg" alt="Card image">
+            <div class="card-img-overlay">
+              <h5 class="card-title pull-left">Destination 1</h5><br><br>
+              <p class="card-text"></p>
+              <p class="card-text"></p>
+            </div><br>
+            <div class="php">
+              <i class="fas fa-user-circle fa-3x"></i>
+              <i class="fas fa-user-circle fa-3x"></i>
+              <i class="fas fa-user-circle fa-3x"></i>
+            </div><br>
+              <small class="text-muted">Voir détails</small></p><br>
+        </div>
+      </div>
+        <div class="col-md-4">         
+          <div class="card">
+          <p class=""><small class=""></small></p><br><br>
+          <img class="card-img" src="images/groupe3.jpg" alt="Card image">
+          <div class="card-img-overlay">
+          <h5 class="card-title pull-left">Destination 2</h5><br><br>
+          <p class="card-text"></p>
+          <p class="card-text"></p>
+          </div><br>
+          <p class="card-text">
+           <i class="fas fa-user-circle fa-3x"></i>
+            <i class="fas fa-user-circle fa-3x"></i>
+            <i class="fas fa-user-circle fa-3x"></i><br><br><small class="text-muted">Voir détails </small></p><br>
+           </div>
+            </div>
+             
+            <div class="col-md-4">         
+             <div class="card">
+                  <p class=""><small class=""></small></p><br><br>
+                  <img class="card-img" src="images/groupe1.jpg" alt="Card image">
+                  <div class="card-img-overlay">
+                    <h5 class="card-title pull-left">Destination 3</h5><br><br>
+                    <p class="card-text"></p>
+                    <p class="card-text"></p>
+                  </div><br>
+                 
+                  <p class="card-text">
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i><br><br><small class="text-muted">Voir détails </small></p><br>
+              </div><br>
+          
+              </div><br><br>
+            </div>
+
+            <div class="row"><br>
+            
+               <div class="col-md-4"> 
+                <div class="card">
+                  <p class=""><small class=""></small></p><br><br>
+                  <img class="card-img" src="images/nice.jpg" alt="Card image">
+                  <div class="card-img-overlay">
+                    <h5 class="card-title pull-left">Destination 4</h5><br><br>
+                    <p class="card-text"></p>
+                    <p class="card-text"></p>
+                  </div><br>
+                 
+                  <p class="card-text">
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i><br><br><small class="text-muted">Voir détails </small></p><br>
+              </div><br>    
+                 
+              </div><br>
+
+           <div class="col-md-4">         
+            <div class="card">
+                  <p class=""><small class=""></small></p><br><br>
+                  <img class="card-img" src="images/alpes.jpg" alt="Card image">
+                  <div class="card-img-overlay">
+                    <h5 class="card-title pull-left">Destination 5</h5><br><br>
+                    <p class="card-text"></p>
+                    <p class="card-text"></p>
+                  </div><br>
+                 
+                  <p class="card-text">
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i><br><br><small class="text-muted">Voir détails </small></p><br>
+              </div>
+          
+              </div><br>
+          
+              
+            <div class="col-md-4">         
+              <div class="card">
+                  <p class=""><small class=""></small></p><br><br>
+                  <img class="card-img" src="images/loire.jpg" alt="Card image">
+                  <div class="card-img-overlay">
+                    <h5 class="card-title pull-left">Destination 6</h5><br><br>
+                    <p class="card-text"></p>
+                    <p class="card-text"></p>
+                  </div><br>
+                 
+                  <p class="card-text">
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <i class="fas fa-user-circle fa-3x"></i><br><br><small class="text-muted">Voir détails </small></p><br>
+              </div>
+
+
+              </div><br>
+            </div>
+            
+  </div><br>
+  <div class="container text-center">
+    <a class="btn btn-primary" href="" >Trouver votre groupe de rêve</a>
+  </div>
+        
+</section>
+
+
+
+						<!-- FOOTER 1 -->
+
+<footer id="agence">
+	<div class="container">
+        <div class="row ">
+            <div class="col-lg-6">         
+              <h5>AGENCE A PARIS 14e</h5>
+              <hr>
+              <p>Zone en déconstruction Web Farce 3<br>01.66.66.66.66<br>pasdinfo@partez.com</p>
+            </div>
+            <div class="col-md-6" id="zoneagence">         
+           <div id="titre4" class="">Une Question ?</div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+						<!-- FOOTER 2 -->
+
+
+<footer id="about">
+	<div class="container">
+          <div class="row ">
+            <div class="col-md-3">         
+               <hr>
+              <a href="">About us</a><br>
+              <a href="">Careers</a><br>
+              <a href="">Help</a><br>
+              <a href="">Press</a><br>
+            </div>
+            <div class="col-md-3">         
+                  <hr>
+              <a href="">Terms</a><br>
+              <a href="">Payment</a><br>
+              <a href="">Shipping</a><br>
+              <a href="">Contact</a><br>
+            </div>
+            <div class="col-md-3">         
+                 <hr>
+              <a href="">HISTOIRE</a><br>
+              <a href="">DECORATIONS</a><br>
+              <a href="">ANIMATIONS</a><br>
+              
+            </div>
+            <div class="col-md-3" id="aboutdroit"> 
+            <hr><br><div id="test"></div>        
+               <i class="fab fa-facebook-square fa-3x"> </i>
+               <i class="fab fa-instagram fa-3x"> </i>
+               <i class="fab fa-linkedin fa-3x"> </i>
+            </div>
+          </div>
+        </div><br>
+           <div class="container">
+     			 <div class="row">
+     			 	
+               			<div class="rights"><i class="far fa-copyright"></i> 2018 CollectifMétissé All rights reserved</div>
+                	
+         		</div>
+         	</div>
+
+</footer>
+
+     <!-- JQUERY / BOOTSTRAP / JavaScript en dernier / -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+	
+</body>
+</html>
