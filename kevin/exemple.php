7818bb41b37e5b63a44a06abb57ff535d217a02d
kevin92320 <cuillandrek@gmail.com>
2018-05-10 11:32:18 +0200

    exemple page connexion

---
+++ kevin/exemple.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+
+	 <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- Mon style -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Black+Han+Sans" rel="stylesheet">
+
+</head>
+<body>
+								       <!-- NAV -->
+	     
+	 <nav class="navbar navbar-expand-lg navbar-light " id="mynav">
+		<a class="navbar-brand pull-sm-left" id="elementgauche" href="index.php">HexaGO </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedCont2ent">
+      <ul class="navbar-nav mx-auto ">
+      <li class="nav-item ">
+      <li class="nav-item" >
+      <a class="nav-link " href="concept.php">Concept</a>
+      </li>
+      <li class="nav-item" >
+      <a class="nav-link " href="#decoration">Membres</a>
+      </li>
+      <li class="">
+      <a class="nav-link " href="#animations">Ventes en cours</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link " href="index.php">Témoignages</a>
+      </li>   
+      </ul>
+      <a class="navbar-brand pull-sm-right" id="elementdroite" href="exemple.php">Préparer mon voyage</a>
+      <a class="navbar-brand pull-sm-right" id="elementdroite" href="profil.php">Mon profil</a>
+      </div>
+	 </nav>
+
+                      <!-- CHOISISSEZ VOTRE GROUPE -->
+
+<section id="histoire">
+  <div class="container">
+    <form method="GET">
+      <div class="form-group">
+      <label for="exampleInputEmail1"></label>
+      <input type="text" class="form-control" aria-describedby="" placeholder="Tapez votre recherche">
+      <small class="form-text text-muted">Faites une recherche libre</small>
+      </div>
+    </form>
+  </div>
+	<div class="container text-center">
+		<h2 class="font-30"><span>C</span>hoisisser votre groupe </h2><br><br>
+    <div class="row "><br>
+    <div class="col-md-6">         
+    <img class="card-img-top" src="images/groupe1.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Voyages toutes générations</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Groupe crée par JP54 il y a 15 heures</small></p>
+    </div><br>
+    </div><br>
+    <div class="col-md-6">         
+    <img class="card-img-top" src="images/groupe2.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Hors du commun</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Groupe crée par Marry il y a 2 heures</small></p>
+    </div>
+    </div><br>
+    </div>
+    <div class="row "><br>
+    <div class="col-md-6">         
+    <img class="card-img-top" src="images/groupe3.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Randonnées</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Groupe crée par John il y a 25 heures</small></p>
+    </div>
+    </div><br>
+    <div class="col-md-6">         
+    <img class="card-img-top" src="images/groupe4.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Groupes de potes</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Groupe crée par Fred il y a 5 heures</small></p>
+    </div>
+    </div>
+    </div>
+  </div>
+  </div>
+</section>
+
+                <!-- CREER VOTRE GROUPE -->
+
+<section id="collegue">
+  <div class="container text-center">
+    <h2 class="font-30"><span>C</span>reer votre groupe </h2><br><br><br><br>
+    <div class="row text-center">
+    </div>
+    <div class="row">
+    <select class="form-control">
+      <option>--Choisissez votre région--</option><br>
+      <option>Dordogne</option><br>
+      <option>Hérault</option><br>
+      <option>Corse</option><br>
+      <option>Alpes</option><br>
+      </select><br><br><br>
+    <button type="submit" class="btn btn-primary mb-2">Confirmer</button>
+    </div>
+  </div>
+  </section><br><br>
+
+
+        <!--   CHOISSISSEZ VOS AMIS DE VOYAGES -->
+
+
+    <section id="histoire">
+      <div class="container text-center">
+        <h2 class="font-30"><span>C</span>hoisisser vos amis de voyage</h2><br><br>
+        <div class="row "><br>
+        <div class="col-md-4">         
+        <img class="card-img-top" src="images/profil1.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Ferdinand le Grand</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Connecté il y a 15 heures</small></p>
+        </div><br>       
+        </div><br>
+        <div class="col-md-4">         
+          <img class="card-img-top" src="images/profil2.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Marry à tout prix</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Connecté il y a 2 heures</small></p>
+          </div>
+        </div><br>
+        <div class="col-md-4">         
+        <img class="card-img-top" src="images/profil1.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">John l'enfoiré</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Connecté il y a 25 heures</small></p>
+        </div>
+        </div><br>
+        </div>
+      </div>
+    </section>
+
+
+                              <!-- FOOTER 1 -->
+
+<footer id="agence">
+	<div class="container">
+    <div class="row ">
+      <div class="col-lg-6">         
+        <h5>AGENCE A PARIS 14e</h5>
+        <hr>
+        <p>Zone en déconstruction Web Farce 3<br>01.66.66.66.66<br>pasdinfo@partez.com</p>
+      </div>
+      <div class="col-md-6" id="zoneagence">         
+        <div id="titre4" class="">Une Question ?</div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+		                				<!-- FOOTER 2 -->
+
+
+      <footer id="about">
+	     <div class="container">
+          <div class="row ">
+            <div class="col-md-3">         
+               <hr>
+              <a href="">About us</a><br>
+              <a href="">Careers</a><br>
+              <a href="">Help</a><br>
+              <a href="">Press</a><br>
+            </div>
+            <div class="col-md-3">         
+                  <hr>
+              <a href="">Terms</a><br>
+              <a href="">Payment</a><br>
+              <a href="">Shipping</a><br>
+              <a href="">Contact</a><br>
+            </div>
+            <div class="col-md-3">         
+                 <hr>
+              <a href="">HISTOIRE</a><br>
+              <a href="">DECORATIONS</a><br>
+              <a href="">ANIMATIONS</a><br>
+              
+            </div>
+            <div class="col-md-3" id="aboutdroit"> 
+            <hr><br><div id="test"></div>        
+               <i class="fab fa-facebook-square fa-3x"> </i>
+               <i class="fab fa-instagram fa-3x"> </i>
+               <i class="fab fa-linkedin fa-3x"> </i>
+            </div>
+          </div>
+        </div><br>
+        <div class="container">
+     			<div class="row">
+     			<div class="rights"><i class="far fa-copyright"></i> 2018 CollectifMétissé All rights reserved</div>	
+         	</div>
+        </div>
+      </footer>
+
+     <!-- JQUERY / BOOTSTRAP / JavaScript en dernier / -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
