45ee72e1a6112cdf4fa3a1c999830471fe7dbc97
HvNelric <37754127+HvNelric@users.noreply.github.com>
2018-05-11 02:30:51 +0200

    Home page complete
    il ne manque plus que le footer

---
+++ react-site/src/pages/Footer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../css/footer.css';
+
+class Footer extends Component{
+    render() {
+        return(
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <h2>FOOTER</h2>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
--- react-site/src/pages/UsersIndex.js
+++ react-site/src/pages/UsersIndex.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import '../css/users-index.css';
 
+import profil01 from '../img/img-users/user-01.jpg';
+
 class UsersIndex extends Component{
     render() {
         return(
@@ -11,16 +13,77 @@ class UsersIndex extends Component{
                         Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
                     </p>
                     <div className="row mt-5">
-                        <div className="col-6">
-                            <div className="user-container">
+                        <div className="col-6 col-md-4 mt-4">
+                            <div className="user-container d-flex justify-content-start"> {/** User-container **/}
                                 <div className="user-img">
-                                    <img src="" alt=""></img>
+                                    <img src={profil01} alt="image du profil"></img>
                                 </div>
-                                <h5 className="">Prénom <span>Nom</span></h5>
-                            </div>
+                                <div className="user-content">
+                                    <h5 className="user-lastname">Prénom</h5>
+                                    <p className="user-destination"><span>Destination : </span>Cassis</p>
+                                    <p className="user-comment">bla bla</p>
+                                </div>
+                            </div> {/** /User-container **/}
                         </div>
-                        <div className="col-6">
-
+                        <div className="col-6 col-md-4 mt-4">
+                            <div className="user-container d-flex justify-content-start"> {/** User-container **/}
+                                <div className="user-img">
+                                    <img src={profil01} alt="image du profil"></img>
+                                </div>
+                                <div className="user-content">
+                                    <h5 className="user-lastname">Prénom</h5>
+                                    <p className="user-destination"><span>Destination : </span>Cassis</p>
+                                    <p className="user-comment">bla bla</p>
+                                </div>
+                            </div> {/** /User-container **/}
+                        </div>
+                        <div className="col-6 col-md-4 mt-4">
+                            <div className="user-container d-flex justify-content-start"> {/** User-container **/}
+                                <div className="user-img">
+                                    <img src={profil01} alt="image du profil"></img>
+                                </div>
+                                <div className="user-content">
+                                    <h5 className="user-lastname">Prénom</h5>
+                                    <p className="user-destination"><span>Destination : </span>Cassis</p>
+                                    <p className="user-comment">bla bla</p>
+                                </div>
+                            </div> {/** /User-container **/}
+                        </div>
+                        <div className="col-6 col-md-4 mt-4">
+                            <div className="user-container d-flex justify-content-start"> {/** User-container **/}
+                                <div className="user-img">
+                                    <img src={profil01} alt="image du profil"></img>
+                                </div>
+                                <div className="user-content">
+                                    <h5 className="user-lastname">Prénom</h5>
+                                    <p className="user-destination"><span>Destination : </span>Cassis</p>
+                                    <p className="user-comment">bla bla</p>
+                                </div>
+                            </div> {/** /User-container **/}
+                        </div>
+                        <div className="col-6 col-md-4 mt-4">
+                            <div className="user-container d-flex justify-content-start"> {/** User-container **/}
+                                <div className="user-img">
+                                    <img src={profil01} alt="image du profil"></img>
+                                </div>
+                                <div className="user-content">
+                                    <h5 className="user-lastname">Prénom</h5>
+                                    <p className="user-destination">Destination : Cassis</p>
+                                    <p className="user-comment">bla bla</p>
+                                </div>
+                            </div> {/** /User-container **/}
+                        </div>
+                        <div className="col-6 col-md-4 mt-4">
+                            <div className="user-container d-flex justify-content-start"> {/** User-container **/}
+                                <div className="user-img">
+                                    <img src={profil01} alt="image du profil"></img>
+                                </div>
+                                <div className="user-content">
+                                    <h5 className="user-lastname">Prénom</h5>
+                                    <p className="user-destination"><span>Destination : </span>Cassis</p>
+                                    <p className="user-comment">bla blabla bla</p>
+                                </div>
+                            </div> {/** /User-container **/}
                         </div>
                     </div>
                 </div>
