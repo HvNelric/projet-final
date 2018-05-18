47a031335e149ae2bf0534b70d23685bb848b0a0
HvN <37754127+HvNelric@users.noreply.github.com>
2018-05-18 17:57:03 +0200

    sym

--- symfony-site/src/Controller/ProfilController.php
+++ symfony-site/src/Controller/ProfilController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\InscriptionType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class ProfilController
@@ -32,18 +34,57 @@ class ProfilController extends Controller
     /**
      * @Route("/edit")
      */
-    public function edit(Request $request) {
+    public function edit(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
 
-        $user = $this->getUser();
-
-       $form = $this->createForm(InscriptionType::class, $user);
+       $form = $this->createForm(InscriptionType::class);
        $form->handleRequest($request);
-       
+
+       $user = $this->getUser();
+       dump($user->getProfilImg());
+
+       if($form->isSubmitted()) {
+           if($form->isValid()) {
+
+               /**
+                * @var UploadedFile|null
+                */
+               $image = $user->getProfilImg();
+
+               if(!is_null($image)) {
+                   $filename = uniqid() . '.' . $image->guessExtension();
+
+                   $image->move(
+                       $this->getParameter('upload_dir'),
+                       $filename
+                   );
+
+                   $user->setProfilImg($filename);
+               }
+
+               $password = $passwordEncoder->encodePassword(
+                   $user,
+                   $user->getPlainPassword()
+               );
+               $user->setPassword($password);
+               dump($user);
+
+               $em = $this->getDoctrine()->getManager();
+               $em->persist($user);
+               $em->flush();
+
+               $this->addFlash('succes', 'Votre a été modifié');
+               return $this->redirectToRoute('app_profil_index');
+           } else {
+               $this->addFlash('error', 'Le formulaire contient des erreurs');
+           }
+       }
+
 
         return $this->render(
             'logged/edit.html.twig',
             [
-                'form' => $form->createView()
+                'form' => $form->createView(),
+                'user' => $user
             ]
         );
     }
