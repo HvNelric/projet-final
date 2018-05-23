3044582c21f015bc94b7e8919ae360d5d69b7085
HvNelric <37754127+HvNelric@users.noreply.github.com>
2018-05-23 19:07:18 +0200

    reimport symfony

---
+++ symfony-site/src/Controller/AdminController.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Activites;
+use App\Entity\Region;
+use App\Entity\Sejour;
+use App\Form\ActivitesType;
+use App\Form\SejourType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class AdminController
+ * @Route("/admin")
+ */
+class AdminController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        $user = $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository(Sejour::class);
+        $sejours = $repository->findAll();
+
+        $repoAct = $em->getRepository(Activites::class);
+        $activities = $repoAct->findAll();
+
+        $repoRegion = $em->getRepository(Region::class);
+        $regions = $repoRegion->findAll();
+
+        return $this->render(
+            'admin/index.html.twig',
+            [
+                'user' => $user,
+                'sejours' => $sejours,
+                'activities' => $activities,
+                'regions' => $regions
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/add-sejour")
+     */
+    public function addSejour(Request $request) {
+
+        $sejour = new Sejour();
+        $form = $this->createForm(SejourType::class, $sejour);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+
+            if($form->isValid()) {
+
+                $image = $sejour->getVilleImg();
+
+                if(!is_null($image)) {
+
+                    $filename = uniqid() . '.' . $image->guessExtension();
+
+                    $image->move(
+                        $this->getParameter('sejour_dir'), $filename
+                    );
+
+                    $sejour->setVilleImg($filename);
+                }
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($sejour);
+                $em->flush();
+
+                $this->addFlash('succes', 'Votre ville a été ajoutée');
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs');
+            }
+        }
+
+        return $this->render(
+            'admin/sejour.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/add-activites")
+     */
+    public function addActivites(Request $request) {
+
+        $activites = new Activites();
+        $form = $this->createForm(ActivitesType::class, $activites);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+
+            if($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($activites);
+                $em->flush();
+
+                $this->addFlash('sucess', "L'activitée est enregistré");
+                return $this->redirectToRoute('app_admin_addactivites');
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs');
+            }
+        }
+
+        return $this->render(
+            'admin/activites.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/add-region")
+     */
+    public function addRegion(Request $request) {
+
+        $region = new Region();
+
+        $form = $this->createForm(Region::class, $region);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+
+            if($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($region);
+                $em->flush();
+
+                $this->addFlash('sucess', "L'activitée est enregistré");
+
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs');
+            }
+        }
+
+        return $this->render(
+            'admin/region.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     *
+     * @Route("/delete-sejour/{id}")
+     */
+    public function deleteSejour(Sejour $sejour)
+    {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($sejour);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                'La catégorie est supprimée'
+            );
+
+        return $this->redirectToRoute('app_admin_index');
+    }
+
+    /**
+     *
+     * @Route("/delete-activite/{id}")
+     */
+    public function deleteActivites(Activites $activites, $id)
+    {
+        if (($activites->getUserActivites()->isEmpty()) || ($activites->getSejourActivites()->isEmpty())) {
+            $em = $this->getDoctrine()->getManager();
+            $em->find(Activites::class, $id);
+
+
+            $em->remove($activites);
+            $em->flush();
+
+            $this->addFlash(
+                'success', "L'activitée est supprimée");
+        } else {
+            $this->addFlash(
+                'error',
+                "L'activitée ne peut pas être supprimée car elle contient appartient à des utilisateurs ou des séjours "
+            );
+        }
+
+        return $this->redirectToRoute('app_admin_index');
+    }
+
+    /**
+     *
+     * @Route("/delete-region/{id}")
+     */
+    public function deleteRegion(Region $region)
+    {
+        if (($region->getUserRegion()->isEmpty()) && ($region->getSejourRegion()->isEmpty())) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($region);
+            $em->flush();
+
+            $this->addFlash(
+                'success', "La région est supprimée");
+        } else {
+            $this->addFlash(
+                'error', 'La région ne peut pas etre supprimée car elle appartient à des utilisateurs u des séjours'
+            );
+        }
+
+        return $this->redirectToRoute('app_admin_index');
+    }
+}
---
+++ symfony-site/src/Form/InscriptionType.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Activites;
+use App\Entity\Region;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'email',
+                EmailType::class,
+            [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ]
+            )
+            ->add(
+                'plainPassword',
+                RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'first_options' => [
+                        'label' => 'Mot de passe',
+                        'attr' => [
+                            'class' => 'form-control'
+                        ],
+                    ],
+                    'second_options' =>[
+                        'label' => 'Confirmation du mot de passe',
+                        'attr' => [
+                            'class' => 'form-control'
+                        ]
+                    ],
+                ]
+            )
+            ->add(
+                'sexe',
+                    ChoiceType::class,
+                    [
+                        'multiple' => false,
+                        'choices' => [
+                            'Femme' => 'femme',
+                            'Homme' => 'homme'
+                        ],
+                        'attr' => [
+                            'class' => 'form-control'
+                        ]
+                    ]
+                )
+            ->add(
+                'prenom',
+                TextType::class,
+                [
+                    'label' => 'Prénom',
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'nom',
+                TextType::class,
+                [
+                    'label' => 'Nom',
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'age',
+                TextType::class,
+                [
+                    'label' => 'Age',
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'date_dispo',
+                DateType::class,
+                    [
+                        'label' => 'Date de début',
+                        'attr' => [
+                            'class' => 'js-datepicker',
+                            'class' => 'form-control',
+                            'html5' => false,
+                            'widget' => 'single_text'
+                        ]
+                    ]
+            )
+            ->add(
+                'date_fin',
+                DateType::class,
+                [
+                    'label' => 'date fin',
+                    'attr' => [
+                        'class' => 'js-datepicker',
+                        'class' => 'form-control',
+                        'html5' => false,
+                        'widget' => 'single_text'
+                    ]
+                ]
+            )
+            ->add(
+                'activites',
+                EntityType::class,
+                [
+                'class' => Activites::class,
+                'multiple' => true,
+                'expanded' => true,
+                'required' => false,
+                'choice_label' => 'getName',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+                ]
+            )
+            ->add(
+                'region',
+                    EntityType::class,
+                    [
+
+                        'class' => Region::class,
+                        'multiple' => true,
+                        'expanded' => true,
+                        'required' => false,
+                        'choice_label' => 'getRegion',
+                        'attr' => [
+                            'class' => 'form-check-input',
+                            'class' => 'form-control'
+                        ],
+
+                    ]
+                )
+            ->add(
+                'profil_img',
+                    FileType::class,
+                        [
+                            'label' => 'Photo de profil',
+                            'required' => false
+                        ]
+
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'validation_groups' => [
+                'inscription'
+            ]
+        ]);
+    }
+}
